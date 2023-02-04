c483b85f986c823c5d568c58703a6af2dcd25b56
ssg02138 <ssg02138@naver.com>
2023-02-05 00:12:30 +0900

    1316. 그룹 단어 체커

---
+++ 1316.py
@@ -0,0 +1,32 @@
+# 문제
+# 그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다. 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만, aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
+
+# 단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에 단어가 들어온다. 단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
+
+# 출력
+# 첫째 줄에 그룹 단어의 개수를 출력한다.
+
+total_cnt = int(input())
+
+if not ((total_cnt >= 1) and (total_cnt <= 100)):
+	print('total_cnt Exception')
+
+result = 0
+
+for i in range(total_cnt):
+	word = input()
+
+	for idx, val in enumerate(word):
+		if idx == len(word) - 1:
+			result += 1
+			break
+
+		if word[idx] == word[idx+1]:
+			pass
+		elif word[idx] in word[idx+1:]:
+			break
+
+print(result)
